e269ccbd292522dc3e41b624831f890916b30687
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-03-12 16:58:12 +0200

    added & rendered user profile model

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profilesPage, name='profiles'),
+    path('profiles/<str:pk>/', views.userProfile, name='user-profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Profile
 
 def profilesPage(request):
-    return render(request, 'users/profiles.html', context={})
+    profiles = Profile.objects.all()
+    return render(request, 'users/profiles.html', context={'profiles': profiles, })
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    skills_with_bio = profile.skill_set.exclude(description__exact="")
+    skills_without_bio = profile.skill_set.filter(description__exact="")
+
+    return render(request, 'users/user_profile.html', context={
+        'profile': profile,
+        'skills_with_bio': skills_with_bio,
+        'skills_without_bio': skills_without_bio,
+    })
